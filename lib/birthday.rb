7212164867a34f51a0523fb0e6a30551b88df6b0
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-17 05:24:46 +0000

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,20 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids.each do |name,age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
 
+def age_appropriate_birthday(birthday_kids)
+  # add your code snippet here!
+  birthday_kids.each do |name,age|
+    if (age >10)
+      puts "You are too old for this."
+    else
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    end
+  end
+  
+end
 
