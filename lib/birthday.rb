d0bd14d0840d8e52b12334e384b6c989695d50f6
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-17 05:24:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,7 +15,7 @@ def age_appropriate_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |name,age|
     if (age >10)
-      puts "You are too old for this"
+      puts "You are too old for this."
     else
       puts "Happy Birthday #{name}! You are now #{age} years old!"
     end
